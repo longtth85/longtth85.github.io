d5fed9e321eb76ecd10d9b5871d67a9130527a76
Long <longtth85@gmail.com>
2019-10-06 02:21:25 +0200

    Changed slider format

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const imgContainer = document.querySelector('.img-container');
 const slider = document.querySelector('.slider');
 const nav = document.querySelector('nav');
-const headline = document.querySelector('.headline');
 
 const tl = new TimelineMax();
 
@@ -10,25 +9,18 @@ const tl = new TimelineMax();
 tl.fromTo(imgContainer,1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut })
     .fromTo(imgContainer, 1.2, {width: '100%'}, {width: '80%', ease: Power2.easeInOut})
     .fromTo(slider, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.2")
-    .fromTo(headline, 0.5, {opacity: "0"}, {opacity: 1, ease: Power2.easeInOut}, "-=0.5")
     .fromTo(nav, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.2");
 
 
 //Function for text appearing on scroll
 const scrollAppear = () => {
-    const introText = document.querySelector('.intro-text');
     const introText2 = document.querySelector('.intro-text2');
     const introText3 = document.querySelector('.intro-text3');
     const introText4 = document.querySelector('.intro-text4');
-    const introPosition = introText.getBoundingClientRect().top;
     const introPosition2 = introText2.getBoundingClientRect().top;
     const introPosition3 = introText3.getBoundingClientRect().top;
     const introPosition4 = introText4.getBoundingClientRect().top;
     const screenPosition = window.innerHeight / 2;
-
-    if(introPosition < screenPosition){
-        introText.classList.add('intro-text-appear');
-    }
 
     if(introPosition2 < screenPosition){
         introText2.classList.add('intro-text-appear');
